932dc3acc7afb1716d6710f823244786c34da956
Carly Mead <meadcarly16@gmail.com>
2024-07-22 08:39:18 -0600

    Created the methods to find the  area of a circle and a triangle and used the methods to write the answers to the console.

---
+++ Module1Review/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module1Review;
+
+class Program
+{
+    static double areaOfACircle(double radius)
+    {
+        var circleArea = Math.PI*Math.Pow(radius, 2);
+        return circleArea;
+    }
+
+    static double areaOfATriangle(double baselength, double height)
+    {
+        var triangleArea = 0.5 * baselength * height;
+        return triangleArea;
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        double circle = areaOfACircle(3);
+        Console.WriteLine($"The area of your circle is {circle}");
+
+        double triangle = areaOfATriangle(3.0, 3.0);
+        Console.WriteLine($"The area of your triangle is {triangle}");
+    }
+}
