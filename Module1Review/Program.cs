7db11aa18174d7bf6ccb4c5976d6e6d5afd82613
Carly Mead <meadcarly16@gmail.com>
2024-07-22 13:20:02 -0600

    Added do-while, while loops and switch case to allow user input and write to the console

--- Module1Review/Program.cs
+++ Module1Review/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Module1Review;
+﻿using System.Threading.Channels;
+
+namespace Module1Review;
 
 class Program
 {
@@ -8,9 +10,9 @@ class Program
         return circleArea;
     }
 
-    static double areaOfATriangle(double baselength, double height)
+    static double areaOfATriangle(double baseLength, double height)
     {
-        var triangleArea = 0.5 * baselength * height;
+        var triangleArea = 0.5 * baseLength * height;
         return triangleArea;
     }
 
@@ -27,16 +29,99 @@ class Program
     }
     static void Main(string[] args)
     {
-        double circle = areaOfACircle(3);
-        Console.WriteLine($"The area of your circle is {circle}");
+        int radius = 0;
+        int baseLength = 0;
+        int height = 0;
+        int length = 0;
+        int width = 0;
+        int side = 0;
+        var userChoice = 0;
 
-        double triangle = areaOfATriangle(3.0, 3.0);
-        Console.WriteLine($"The area of your triangle is {triangle}");
+        do
+        {
+            Console.WriteLine("Please choose from the options below:");
+            Console.WriteLine("1. Find the area of a circle.");
+            Console.WriteLine("2. Find the area of a triangle.");
+            Console.WriteLine("3. Find the area of a rectangle.");
+            Console.WriteLine("4. Find the area of a square.");
+            Console.WriteLine("5. Exit");
+            while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 1 || userChoice > 5)
+            {
+                {
+                    Console.WriteLine("Invalid input, please enter a valid number");
+                }
+            }
+            
+            switch (userChoice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the radius of your circle.");
+                    while (!int.TryParse(Console.ReadLine(), out radius))
+                    {
+                        Console.WriteLine("Invalid input, please enter a valid number");
+                    }
 
-        double rectangle = areaOfARectangle(4, 5);
-        Console.WriteLine($"The area of your rectangle is {rectangle}");
+                    double circle = areaOfACircle(radius);
+                    Console.WriteLine($"The area of your circle is {circle}.");
+                    break;
 
-        double square = areaOfASquare(5);
-        Console.WriteLine($"The area of your square is {square}");
+                case 2:
+                    Console.WriteLine("Enter the base length of your triangle.");
+                    while (!int.TryParse(Console.ReadLine(), out baseLength))
+                    {
+                        Console.WriteLine("Invalid input, please enter a valid number");
+                    }
+
+                    Console.WriteLine("Enter the height of your triangle.");
+                    while (!int.TryParse(Console.ReadLine(), out height))
+                    {
+                        Console.WriteLine("Invalid input, please enter a valid number");
+                    }
+
+                    double triangle = areaOfATriangle(baseLength, height);
+                    Console.WriteLine($"The area of your triangle is {triangle}");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter the length of your rectangle.");
+                    while (!int.TryParse(Console.ReadLine(), out length))
+                    {
+                        Console.WriteLine("Invalid input, please enter a valid number");
+                    }
+
+                    Console.WriteLine("Enter the width of your rectangle.");
+                    while (!int.TryParse(Console.ReadLine(), out width))
+                    {
+                        Console.WriteLine("Invalid input, please enter a valid number");
+                    }
+
+                    double rectangle = areaOfARectangle(length, width);
+                    Console.WriteLine($"The area of your rectangle is {rectangle}");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter the side of your square.");
+                    while (!int.TryParse(Console.ReadLine(), out side))
+                    {
+                        Console.WriteLine("Invalid input, please enter a valid number");
+                    }
+
+                    double square = areaOfASquare(side);
+                    Console.WriteLine($"The area of your square is {square}");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Thank you for using our Area App!...goodbye.");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid input, please enter a the number of an option listed");
+                    break;
+            }
+
+            Console.WriteLine();
+            
+        } while (userChoice != 5);
+
     }
 }
--- Module1Review/obj/Debug/net8.0/Module1Review.AssemblyInfo.cs
+++ Module1Review/obj/Debug/net8.0/Module1Review.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb9ceb89a8338bab859098860ceac88749cc969")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
